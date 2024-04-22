523cb2e1c140871f36967d0684b128856cbdd436
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-22 16:51:10 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -3,6 +3,6 @@ export CUDA_VISIBLE_DEVICES=0
 python3 -u evaluate_judge.py \
     --model-name-or-path "./models/JudgeLM-7B" \
     --model-type "judgelm" \
-    --data-type "prometheus-ind" \
+    --data-type "llmbar-natural" \
     --eval-batch-size 16 \
     --max-new-token 1024

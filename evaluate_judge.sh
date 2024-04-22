57fe6d518e8238ce56baa935c8b344466ee1d4df
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-22 14:02:26 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Auto-J-13B" \
+    --model-name-or-path "./models/JudgeLM-7B" \
     --model-type "auto-j" \
-    --data-type "auto-j" \
+    --data-type "prometheus-ind" \
     --eval-batch-size 16 \
     --max-new-token 1024
--- evaluate_judge02.sh
+++ evaluate_judge02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Auto-J-13B" \
+    --model-name-or-path "./models/JudgeLM-7B" \
     --model-type "auto-j" \
-    --data-type "judgelm" \
+    --data-type "prometheus-ood" \
     --eval-batch-size 16 \
     --max-new-token 1024

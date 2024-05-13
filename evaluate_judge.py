801b840d9a973c35e458871a084f74b1545f8bde
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-13 20:09:33 +0800

    fix some bugs

--- evaluate_judge.py
+++ evaluate_judge.py
@@ -41,7 +41,7 @@ def build_params():
     parser.add_argument(
         "--data-type",
         type=str,
-        choices=("judgelm", "pandalm", "auto-j", "prometheus-ind", "prometheus-ood", "toxic-chat",
+        choices=("judgelm", "pandalm", "auto-j", "prometheus-ind", "prometheus-ood", "toxic-chat", "salad-bench", "halu-eval"
                  "llmbar-neighbor", "llmbar-natural", "llmbar-gptinst", "llmbar-gptout", "llmbar-manual"),
         default=None,
     )

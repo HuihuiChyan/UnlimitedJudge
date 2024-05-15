2c863894610510e5132d479e42d18ff840e0ae98
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-15 20:51:16 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=7
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/JudgeLM-7B" \
+    --model-name-or-path "./models/Auto-J-13B" \
     --prompt-type "vanilla" \
-    --model-type "judgelm" \
-    --data-type "salad-bench" \
+    --model-type "auto-j" \
+    --data-type "halu-eval-summary" \
     --eval-batch-size 16 \
     --max-new-token 1024

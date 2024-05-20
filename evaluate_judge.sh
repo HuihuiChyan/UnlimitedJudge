22ba24e6b4a77eba01a1092e1c2ed1d2770c6291
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-20 12:29:57 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Prometheus-7B" \
+    --model-name-or-path "./models/PandaLM-7B" \
     --prompt-type "vanilla" \
-    --model-type "prometheus" \
-    --data-type "toxic-chat" \
+    --model-type "pandalm" \
+    --data-type "salad-bench" \
     --max-new-token 1024

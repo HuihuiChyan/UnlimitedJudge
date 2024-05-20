977d6109d1da945c8fcae5abbc469f08bde9472e
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-20 10:55:39 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Auto-J-13B" \
+    --model-name-or-path "./models/Prometheus-7B" \
     --prompt-type "vanilla" \
-    --model-type "auto-j" \
+    --model-type "prometheus" \
     --data-type "toxic-chat" \
     --eval-batch-size 16 \
     --max-new-token 1024

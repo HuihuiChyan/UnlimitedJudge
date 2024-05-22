1582aef1ed3c5f1683d394941648978fadf7f4e8
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-22 17:44:42 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4
 python3 -u evaluate_judge.py \
     --model-name-or-path "./models/PandaLM-7B" \
-    --prompt-type "vanilla" \
+    --prompt-type "icl" \
     --model-type "pandalm" \
-    --data-type "salad-bench" \
+    --data-type "pandalm" \
     --max-new-token 1024

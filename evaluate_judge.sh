4d2a0063f3f024b36a5920a032a11b91385fb4cd
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-23 11:36:24 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Auto-J" \
+    --model-name-or-path "./models/Auto-J-13B" \
     --prompt-type "icl" \
     --model-type "auto-j" \
     --data-type "pandalm" \

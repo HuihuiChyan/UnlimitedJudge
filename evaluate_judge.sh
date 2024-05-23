e514520789d73a138d76c2dbfcbabdebd109e215
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-23 13:40:06 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -4,5 +4,5 @@ python3 -u evaluate_judge.py \
     --model-name-or-path "./models/Auto-J-13B" \
     --prompt-type "icl" \
     --model-type "auto-j" \
-    --data-type "pandalm" \
+    --data-type "judgelm" \
     --max-new-token 1024

a0d1c69b8ac12d0aed3caba16f386d84a4a59885
huanghui_dxm <huanghui@duxiaoman.com>
2024-05-23 11:19:11 +0800

    fix some bugs

--- evaluate_judge02.sh
+++ evaluate_judge02.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=1
 python3 -u evaluate_judge.py \
     --model-name-or-path "./models/JudgeLM-7B" \
-    --prompt-type "vanilla" \
+    --prompt-type "icl" \
     --model-type "judgelm" \
     --data-type "judgelm" \
     --max-new-token 1024

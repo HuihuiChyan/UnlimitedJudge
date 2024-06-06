6817808ede6a52faaaf8c1ec29f43250f14f145e
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-06 18:52:29 +0800

    delete ds store

--- evaluate_gpt02.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python -u evaluate_gpt.py \
-    --model-type "gpt-3.5-turbo-0125" \
-    --prompt-type "cot" \
-    --data-type "judgelm" \
-    --data-path ./data \
-    --multi-process True \
-    --save-logit True
--- evaluate_judge02.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
-python3 -u evaluate_judge.py \
-    --model-name-or-path "./models/Auto-J-13B" \
-    --prompt-type "icl" \
-    --model-type "auto-j" \
-    --data-type "judgelm" \
-    --max-new-token 1024

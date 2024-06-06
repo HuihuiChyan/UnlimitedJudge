c1ee9ecae2a5014b4c83289f5a977644ddfef75e
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-06 13:55:54 +0800

    fix some bugs

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -4,5 +4,5 @@ python3 -u evaluate_judge.py \
     --model-name-or-path "./models/Auto-J-13B" \
     --prompt-type "vanilla" \
     --model-type "auto-j" \
-    --data-type "halu-eval-dialogu" \
+    --data-type "halu-eval-dialogue" \
     --max-new-token 1024

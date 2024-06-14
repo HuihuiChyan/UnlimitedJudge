9c3f531378c2bc72919216e23cac1bf214171483
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-14 15:58:03 +0800

    fix some bugs

--- cal_reliability.sh
+++ cal_reliability.sh
@@ -1,16 +1,16 @@
 export CUDA_VISIBLE_DEVICES=6
 
-MODEL_TYPE="auto-j"
+MODEL_TYPE="pandalm"
 DATA_TYPE="salad-bench"
 
-# python3 -u src/cal_reliability.py \
-#     --model-name-or-path "./models/Auto-J-13B" \
-#     --cali-model-name-or-path "./models/llama2-13b-chat-hf/" \
-#     --model-type ${MODEL_TYPE} \
-#     --data-type $DATA_TYPE \
-#     --max-new-token 512 \
-#     --logit-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-logit.jsonl" \
-#     --output-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-relia.json"
+python3 -u src/cal_reliability.py \
+    --model-name-or-path "/home/disk/huanghui/UnlimitedJudge/models/PandaLM-7B" \
+    --cali-model-name-or-path "/home/disk/huanghui/CrossEval-corr/models/llama-7b/" \
+    --model-type ${MODEL_TYPE} \
+    --data-type $DATA_TYPE \
+    --max-new-token 512 \
+    --logit-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-logit.jsonl" \
+    --output-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-relia.json"
 
 python3 -u src/evaluate_reliability.py \
     --model-type ${MODEL_TYPE} \
@@ -18,36 +18,19 @@ python3 -u src/evaluate_reliability.py \
     --logit-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-logit.jsonl" \
     --output-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-relia.json"
 
-DATA_TYPE="salad-bench"
+MODEL_TYPE="judgelm"
 
-# python3 -u src/cal_reliability.py \
-#     --model-name-or-path "/home/disk/huanghui/UnlimitedJudge/models/PandaLM-7B" \
-#     --cali-model-name-or-path "/home/disk/huanghui/CrossEval-corr/models/llama-7b/" \
-#     --model-type "pandalm" \
-#     --data-type $DATA_TYPE \
-#     --max-new-token 1024 \
-#     --logit-file "relia_scores/pandalm/${DATA_TYPE}-logit.jsonl" \
-#     --output-file "relia_scores/pandalm/${DATA_TYPE}-relia.json"
+python3 -u src/cal_reliability.py \
+    --model-name-or-path "/home/disk/huanghui/UnlimitedJudge/models/JudgeLM-7B" \
+    --cali-model-name-or-path "/home/disk/huanghui/CrossEval-corr/models/vicuna-7b/" \
+    --model-type ${MODEL_TYPE} \
+    --data-type $DATA_TYPE \
+    --max-new-token 1024 \
+    --logit-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-logit.jsonl" \
+    --output-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-relia.json"
 
-# python3 -u src/evaluate_reliability.py \
-#     --model-type "pandalm" \
-#     --data-type $DATA_TYPE \
-#     --logit-file "relia_scores/pandalm/${DATA_TYPE}-logit.jsonl" \
-#     --output-file "relia_scores/pandalm/${DATA_TYPE}-relia.json"
-
-# DATA_TYPE="salad-bench"
-
-# python3 -u src/cal_reliability.py \
-#     --model-name-or-path "/home/disk/huanghui/UnlimitedJudge/models/Auto-J-13B" \
-#     --cali-model-name-or-path "/home/disk/huanghui/CrossEval-corr/models/llama2-chat-13b/" \
-#     --model-type "auto-j" \
-#     --data-type $DATA_TYPE \
-#     --max-new-token 1024 \
-#     --logit-file "relia_scores/auto-j/${DATA_TYPE}-logit.jsonl" \
-#     --output-file "relia_scores/auto-j/${DATA_TYPE}-relia.json"
-
-# python3 -u src/evaluate_reliability.py \
-#     --model-type "auto-j" \
-#     --data-type $DATA_TYPE \
-#     --logit-file "relia_scores/auto-j/${DATA_TYPE}-logit.jsonl" \
-#     --output-file "relia_scores/auto-j/${DATA_TYPE}-relia.json"
+python3 -u src/evaluate_reliability.py \
+    --model-type ${MODEL_TYPE} \
+    --data-type $DATA_TYPE \
+    --logit-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-logit.jsonl" \
+    --output-file "relia_scores/${MODEL_TYPE}/${DATA_TYPE}-relia.json"
